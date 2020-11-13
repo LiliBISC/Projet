4b39381659724b263333e87ff0be609a4fd3a68d
Lilian Biscarrat <lilian.bscr@gmail.com>
2020-11-13 22:16:12 +0100

    first commit
    first commit

---
+++ src/projet/Customer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet;
+
+/**
+ *
+ * @author trani
+ */
+public class Customer {
+    protected String name;
+    protected String surname;
+
+    public Customer(String name,String surname){
+        this.name=name;
+        this.surname=surname;
+    }
+   
+    
+}
